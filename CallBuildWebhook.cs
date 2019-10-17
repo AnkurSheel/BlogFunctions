eb9d49b95f0da2699ff2d4d0345a3a984003d5eb
Ankur Sheel <ankur.sheel@pushpay.com>
2019-10-17 16:24:49 +1300

    add a timer trigger for everyday at 2:00 pm

---
+++ CallBuildWebhook.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace BlogFunctions
+{
+	public static class CallBuildWebhook
+	{
+		// {second=0} {minute=15} {hour=10} {day} {month} {day-of-week=(2=Tuesday)}
+		private const string TimerSchedule = "0 00 14 * * *";
+		private static readonly HttpClient _client = new HttpClient();
+
+		[FunctionName("CallBuildWebhook")]
+		public static async Task RunAsync([TimerTrigger(TimerSchedule)] TimerInfo myTimer, ILogger log)
+		{
+			log.LogInformation($"Tiggering Blog rebuild at: {DateTime.Now}");
+
+			var buildHookUrl = Environment.GetEnvironmentVariable("BuildHookUrl");
+
+			await _client.PostAsJsonAsync(buildHookUrl, "{}");
+
+			log.LogInformation($"Blog rebuild triggered successfully at: {DateTime.Now}");
+		}
+	}
+}
